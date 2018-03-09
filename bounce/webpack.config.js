9dd0fb85cba94a0eb9e545d3bfe54a403c49938a
Tilo Dietrich <tilo.dietrich@mynewsdesk.com>
2018-03-09 15:42:13 +0100

    Add babel

--- bounce/webpack.config.js
+++ bounce/webpack.config.js
@@ -1,15 +1,13 @@
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-transform-class-properties']
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
         }
       }
-    }
-  ]
+    ]
+  }
 }
