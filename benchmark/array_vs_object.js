22fd2ed1e9e16258c66f4d6fc769514cd2ad3dfd
Tilo Dietrich <tilodietrich@posteo.de>
2018-03-11 23:43:54 +0100

    Wrap tests in classes

--- benchmark/array_vs_object.js
+++ benchmark/array_vs_object.js
@@ -1,19 +1,7 @@
 const Benchmark = require('benchmark');
 
-// array
-const buildArray = (zero, one, two) => [zero, one, two];
-
-// object
-const buildObject = (zero, one, two) => {
-  return {
-    zero: zero,
-    one: one,
-    two: two
-  };
-};
-
-// class
-class Klass {
+// direct assignment
+class WithDirectAssignment {
   constructor(zero, one, two) {
     this.zero = zero;
     this.one = one;
@@ -21,27 +9,61 @@ class Klass {
   }
 }
 
+// array
+class WithArray {
+  constructor(zero, one, two) {
+    this.array = [zero, one, two];
+  }
+}
+
+// object
+class WithObject {
+  constructor(zero, one, two) {
+    this.object = { zero: zero, one: one, two: two };
+  }
+}
+
+// inner class
+class WithInnerClass {
+  constructor(zero, one, two) {
+    this.innerClass = new WithDirectAssignment(zero, one, two);
+  }
+}
+
 new Benchmark.Suite()
+  .add('no class', () => {
+    const zero = 2;
+    const one = 4;
+    const two = 8;
+    return zero + one + two + zero;
+  })
+  .add('direct assignment', () => {
+    const directAssignment = new WithDirectAssignment(2, 4, 8);
+    return directAssignment.zero +
+      directAssignment.one +
+      directAssignment.two +
+      directAssignment.zero;
+  })
   .add('array', () => {
-    const array = buildArray(2, 4, 8);
-    array[0];
-    array[1];
-    array[2];
-    array[0];
+    const withArray = new WithArray(2, 4, 8);
+    return withArray.array[0] +
+      withArray.array[1] +
+      withArray.array[2] +
+      withArray.array[0];
   })
   .add('object', () => {
-    const object = buildObject(2, 4, 8);
-    object.zero;
-    object.one;
-    object.two;
-    object.zero;
+    const withObject = new WithObject(2, 4, 8);
+    return withObject.object.zero +
+      withObject.object.one +
+      withObject.object.two +
+      withObject.object.zero;
   })
   .add('class', () => {
-    const klass = new Klass(2, 4, 8);
-    klass.zero;
-    klass.one;
-    klass.two;
-    klass.zero;
+    const withInnerClass = new WithInnerClass(2, 4, 8);
+    return withInnerClass.innerClass.zero +
+      withInnerClass.innerClass.one +
+      withInnerClass.innerClass.two +
+      withInnerClass.innerClass.zero;
   })
   .on('cycle', event => console.log(String(event.target)))
   .run();
