8008cc5ca7df8d2d0366e0a5ee9c32693ad62cbf
Tilo Dietrich <tilodietrich@posteo.de>
2018-03-11 23:44:09 +0100

    Add test for array traversal

---
+++ benchmark/array_traversal.js
@@ -0,0 +1,23 @@
+const Benchmark = require('benchmark');
+
+const array = [];
+for (let i = 0; i < 100; ++i) array.push(i * 2);
+
+new Benchmark.Suite()
+  .add('foo-in-loop', () => {
+    let sum = 0;
+    for (let x in array) sum += x;
+    return sum;
+  })
+  .add('forEach', () => {
+    let sum = 0;
+    array.forEach(x => sum += x);
+    return sum;
+  })
+  .add('for with index', () => {
+    let sum = 0;
+    for (let j = 0; j < 100; ++j) sum += array[j];
+    return sum;
+  })
+  .on('cycle', event => console.log(String(event.target)))
+  .run();
