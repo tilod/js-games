255992605577fdc71992c8b6f7a91882ad7f5494
Tilo Dietrich <tilodietrich@posteo.de>
2018-03-16 21:34:40 +0100

    Wording

--- benchmark/array_traversal.js
+++ benchmark/array_traversal.js
@@ -1,15 +1,10 @@
 const Benchmark = require('benchmark');
 
 const array = [];
-for (let i = 0; i < 100; ++i) array.push(i * 2);
+for (let i = 0; i < 100; ++i) array.push(i);
 
 new Benchmark.Suite()
-  .add('foo in loop     ', () => {
-    let sum = 0;
-    for (let x in array) sum += x;
-    return sum;
-  })
-  .add('foo of loop     ', () => {
+  .add('foo...of loop   ', () => {
     let sum = 0;
     for (let x of array) sum += x;
     return sum;
