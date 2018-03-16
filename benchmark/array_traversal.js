8d4c1af7d501246839cf6023255caa441fbd3337
Tilo Dietrich <tilodietrich@posteo.de>
2018-03-16 21:28:50 +0100

    Change formating

--- benchmark/array_traversal.js
+++ benchmark/array_traversal.js
@@ -4,27 +4,27 @@ const array = [];
 for (let i = 0; i < 100; ++i) array.push(i * 2);
 
 new Benchmark.Suite()
-  .add('foo in loop', () => {
+  .add('foo in loop     ', () => {
     let sum = 0;
     for (let x in array) sum += x;
     return sum;
   })
-  .add('foo of loop', () => {
+  .add('foo of loop     ', () => {
     let sum = 0;
     for (let x of array) sum += x;
     return sum;
   })
-  .add('forEach', () => {
-    let sum = 0;
-    array.forEach(x => sum += x);
-    return sum;
-  })
-  .add('for with index', () => {
+  .add('for with index  ', () => {
     let sum = 0;
     for (let j = 0; j < array.length; ++j) sum += array[j];
     return sum;
   })
-  .add('iterator', () => {
+  .add('forEach         ', () => {
+    let sum = 0;
+    array.forEach(x => sum += x);
+    return sum;
+  })
+  .add('iterator        ', () => {
     let sum = 0;
     const iterator = array[Symbol.iterator]();
     while(true) {
