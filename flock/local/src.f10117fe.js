9e80dcde2b35f4a75e29b5827e5590ee6470b57a
Tilo Dietrich <tilo.dietrich@mynewsdesk.com>
2018-12-11 15:02:15 +0100

    Change Parcel configuration

--- flock/local/src.f10117fe.js
+++ flock/local/src.f10117fe.js
@@ -605,7 +605,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52956" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53111" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -748,4 +748,4 @@ function hmrAccept(bundle, id) {
   });
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js","src/index.ts"], null)
-//# sourceMappingURL=src.f10117fe.map
+//# sourceMappingURL=/src.f10117fe.map
